d98be6edffaf3293ba8a07b0a2c5ab4743caa7e3
Leonid Sliusar <loslyusar@gmail.com>
2023-04-12 07:50:51 +0300

    + admin panel

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Menu
+from django import forms
 
-# Register your models here.
+label_for_parent_title = 'Родительское меню'
+class MenuFormAdmin(forms.ModelForm):
+    parent_title = forms.ModelChoiceField(queryset=Menu.objects.all(), label=label_for_parent_title, required=False)
+
+    class Meta:
+        model = Menu
+        fields = ['title']
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ('title', 'parent_title')
+    search_fields = ('title', 'parent_title')
+    ordering = ('title',)
+    form = MenuFormAdmin
+
+    def parent_title(self, obj):
+        obj.parent_title = Menu.objects.get(pk=obj.parent_id).title
+        return obj.parent_title if obj.parent_title else "-"
+    parent_title.short_description = label_for_parent_title
+
+
+    def save_model(self, request, obj, form, change):
+        if form.cleaned_data['parent_title']:
+            parent_title = form.cleaned_data['parent_title']
+            parent = Menu.objects.get(title=parent_title)
+            obj.parent_id = parent.pk
+        else:
+            obj.parent_id = None
+        obj.save()
+
+admin.site.register(Menu, MenuAdmin)
--- menu/models.py
+++ menu/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
 class Menu(models.Model):
-    title = models.CharField(max_length=100, blank=False)
+    title = models.CharField(max_length=100, blank=False, verbose_name='Заголовок меню')
     parent_id = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+        ordering = ['title']
